9e7f3279e60660a6c60badc0a51328718357359a
Asif Ahammad <asifahammed359@gmail.com>
2024-03-25 17:58:11 +0600

    add : crt sqlStatement function for sql queary prepeard

--- src/main/java/com/example/bubt/controllers/AdminControllers/AdStudentController.java
+++ src/main/java/com/example/bubt/controllers/AdminControllers/AdStudentController.java
@@ -45,85 +45,94 @@ public class AdStudentController {
         String address = StFdAddress.getText();
         String city = StFdCity.getText();
 
-        try {
-            SqlDB reqDB = new SqlDB();
-            Connection connectDB = reqDB.getDatabaseLink();
-            String Role = "Student";
+        if(name.isEmpty() || email.isEmpty() || subject.isEmpty() || intake.isEmpty() || phone.isEmpty() || section.isEmpty() || password.isEmpty()|| address.isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Text field is empty?");
+            alert.showAndWait();
+        }
+        else {
+            try {
+                SqlDB reqDB = new SqlDB();
+                Connection connectDB = reqDB.getDatabaseLink();
 
-            String sqlUser = "INSERT INTO usertable (Name, Email, Password, Role) VALUES (?, ?, ?, ?)";
+                String Role = "Student";
+                String sqlUser = "INSERT INTO usertable (Name, Email, Password, Role) VALUES (?, ?, ?, ?)";
+                PreparedStatement userStatement = reqDB.Statement(sqlUser);
+                userStatement.setString(1, name);
+                userStatement.setString(2, email);
+                userStatement.setString(3, password);
+                userStatement.setString(4, Role);
+                int userInserted = userStatement.executeUpdate();
 
-            PreparedStatement userStatement = connectDB.prepareStatement(sqlUser);
-            userStatement.setString(1, name);
-            userStatement.setString(2, email);
-            userStatement.setString(3, password);
-            userStatement.setString(4, Role);
-
-            int userInserted = userStatement.executeUpdate();
-
-            if (userInserted > 0)
-            {
-                String getUserIdQuery = "SELECT id FROM usertable WHERE Email='"+email+"' AND Password='"+password+"'";
-
-                PreparedStatement userIdStatement = connectDB.prepareStatement(getUserIdQuery);
-                ResultSet getUserId = userIdStatement.executeQuery();
-
-                if(getUserId.next())
+                if (userInserted > 0)
                 {
-                    int userID = getUserId.getInt("id");
+                    String getUserIdQuery = "SELECT id FROM usertable WHERE Email='"+email+"' AND Password='"+password+"'";
+                    PreparedStatement userIdStatement = connectDB.prepareStatement(getUserIdQuery);
+                    ResultSet getUserId = userIdStatement.executeQuery();
 
-                    String sql = "INSERT INTO studenttable (user_ID, Name, Email, Intake, Section, Phone, Address, Subject, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                    PreparedStatement statement = connectDB.prepareStatement(sql);
-                    statement.setInt(1, userID);
-                    statement.setString(2, name);
-                    statement.setString(3, email);
-                    statement.setString(4, intake);
-                    statement.setString(5, section);
-                    statement.setString(6, phone);
-                    statement.setString(7, address);
-                    statement.setString(8, subject);
-                    statement.setString(9, password);
-
-                    int rowsInserted = statement.executeUpdate();
-
-                    if (rowsInserted > 0) {
-                         StFdName.setText("");
-                         StFdEmail.setText("");
-                         StFdSubject.setText("");
-                         StFdIntake.setText("");
-                         StFdPhone.setText("");
-                         StFdSection.setText("");
-                         StFdPassword.setText("");
-                         StFdAddress.setText("");
-                         StFdCity.setText("");
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setTitle("Success");
-                        alert.setHeaderText(null);
-                        alert.setContentText("A new record has been inserted successfully.");
-                        alert.showAndWait();
-                    }
-                    else
+                    if(getUserId.next())
                     {
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setTitle("Error");
-                        alert.setHeaderText(null);
-                        alert.setContentText("Some problem to insert New Record");
-                        alert.showAndWait();
+                        int userID = getUserId.getInt("id");
+
+                        String sql = "INSERT INTO studenttable (user_ID, Name, Email, Intake, Section, Phone, Address, Subject, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                        PreparedStatement statement = connectDB.prepareStatement(sql);
+                        statement.setInt(1, userID);
+                        statement.setString(2, name);
+                        statement.setString(3, email);
+                        statement.setString(4, intake);
+                        statement.setString(5, section);
+                        statement.setString(6, phone);
+                        statement.setString(7, address);
+                        statement.setString(8, subject);
+                        statement.setString(9, password);
+
+                        int rowsInserted = statement.executeUpdate();
+
+                        if (rowsInserted > 0) {
+                            StFdName.setText("");
+                            StFdEmail.setText("");
+                            StFdSubject.setText("");
+                            StFdIntake.setText("");
+                            StFdPhone.setText("");
+                            StFdSection.setText("");
+                            StFdPassword.setText("");
+                            StFdAddress.setText("");
+                            StFdCity.setText("");
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                            alert.setTitle("Success");
+                            alert.setHeaderText(null);
+                            alert.setContentText("A new record has been inserted successfully.");
+                            alert.showAndWait();
+                        }
+                        else
+                        {
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                            alert.setTitle("Error");
+                            alert.setHeaderText(null);
+                            alert.setContentText("Some problem to insert New Record");
+                            alert.showAndWait();
+                        }
                     }
                 }
+                else
+                {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("User data don't inserted");
+                    alert.showAndWait();
+                }
+
+                
+            } catch (SQLException e) {
+                System.out.println("Error inserting record: " + e.getMessage());
             }
-            else
-            {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Error");
-                alert.setHeaderText(null);
-                alert.setContentText("User data don't inserted");
-                alert.showAndWait();
-            }
-
-
-
-        } catch (SQLException e) {
-            System.out.println("Error inserting record: " + e.getMessage());
         }
+
+
+
     }
 }
--- src/main/java/com/example/bubt/utils/SqlDB.java
+++ src/main/java/com/example/bubt/utils/SqlDB.java
@@ -3,10 +3,13 @@ package com.example.bubt.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SqlDB {
    public Connection databaseLink ;
+   public String sqlStatement;
+   public PreparedStatement statement;
    public Connection getDatabaseLink () {
 
         String url ="jdbc:mysql://localhost/university" ;
@@ -17,12 +20,26 @@ public class SqlDB {
             Class.forName("com.mysql.jdbc.Driver");
             databaseLink = (Connection) DriverManager.getConnection(url,user,password);
 
-            System.out.println("connect successfully");
+            //System.out.println("connect successfully");
 
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
 
         return databaseLink;
+   }
+
+   public PreparedStatement Statement(String sqlStatement )
+   {
+       this.sqlStatement = sqlStatement;
+       Connection connectDB = getDatabaseLink();
+       try {
+            statement = connectDB.prepareStatement(sqlStatement);
+            return statement ;
+
+       } catch (SQLException e) {
+           throw new RuntimeException(e);
+       }
+
    }
 }
